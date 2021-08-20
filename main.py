502e30cbb8fa2cedbd25946383d98079178b9a02
Bolat Mukashev <ya.ne.angel.kimi@gmail.com>
2021-08-21 02:09:14 +0500

    доработал скачивание фото по url адресу

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from instabot import Bot
 from config import INST_USERNAME, INST_PASSWORD
 
 
-user_url = "https://www.instagram.com/p/CSGTtd9qfyU/?utm_medium=copy_link"
+user_url = "https://www.instagram.com/p/CSwTRIDoJlm/?utm_source=ig_web_copy_linkk"
 
 
 def connect():
@@ -56,6 +56,22 @@ def get_all_photos(my_bot, nickname: str):
         download_all_photos(my_bot, media_id, "img_" + str(e), nickname)
 
 
+def get_photo_by_url(my_bot, url: str):
+    # по url получаем id поста
+    media_id = insta_bot.get_media_id_from_link(user_url)
+    # по id поста получаем информацию о посте
+    media = insta_bot.get_media_info(media_id)[0]
+    # вытаскиваем из информации о посте ссылку на фото
+    if "image_versions2" in media.keys():
+        url = media["image_versions2"]["candidates"][0]["url"]
+        # получаем фото
+        response = requests.get(url)
+        # сохраняем фото
+        with open("photos" + "/" + "test.jpg", "wb") as f:
+            response.raw.decode_content = True
+            f.write(response.content)
+
+
 target_name = "adim0k"
 
 
@@ -63,5 +79,5 @@ if __name__ == "__main__":
     clean_up()
     clean_up(target_name)
     insta_bot = connect()
-    get_all_photos(insta_bot, target_name)
-
+    # get_all_photos(insta_bot, target_name)
+    get_photo_by_url(insta_bot, user_url)
