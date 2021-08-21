78c01cd1dc42ec23f9b48e79813c6635c4e7fe5b
Bolat Mukashev <ya.ne.angel.kimi@gmail.com>
2021-08-21 19:06:55 +0500

    убрал разделение фотографий на папки, теперь это единое хранилище

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import requests
+import time
 from instabot import Bot
 from config import INST_USERNAME, INST_PASSWORD
 
@@ -10,17 +11,18 @@ def connect() -> object:
     Осуществляем вход в инстаграм по логину и паролю
     :return: объект Bot из библиотеки instabot
     """
+    clean_up()
     bot = Bot()
     bot.login(username=INST_USERNAME, password=INST_PASSWORD)
     return bot
 
 
-def clean_up(*args: str, dir_name: str = "config") -> None:
+def clean_up(*args: str, config_dir_name: str = "config") -> None:
     """
     Удаляет ненужные директории при инициализации подключения, иначе подключение не получится
     А так же удаляет директории по запросу
     :param args: удаляет директории по запросу, прописывать названии ненужных дирректорий
-    :param dir_name: по умолчанию удаляет папку config, иначе не произойдет подключение к API инстаграмм
+    :param config_dir_name: по умолчанию удаляет папку config, иначе не произойдет подключение к API инстаграмм
     """
     if args:
         for nickname in args:
@@ -31,7 +33,7 @@ def clean_up(*args: str, dir_name: str = "config") -> None:
                 except OSError as e:
                     print("Error: %s - %s." % (e.filename, e.strerror))
     else:
-        directory = os.path.join(os.getcwd(), dir_name)
+        directory = os.path.join(os.getcwd(), config_dir_name)
         if os.path.exists(directory):
             try:
                 shutil.rmtree(directory)
@@ -39,36 +41,26 @@ def clean_up(*args: str, dir_name: str = "config") -> None:
                 print("Error: %s - %s." % (e.filename, e.strerror))
 
 
-def create_new_directory(dir_name: str) -> None:
-    """
-    Проверить и создать новую директорию
-    :param dir_name: название директории
-    """
-    directory = os.path.join('photos', dir_name)
-    if not os.path.exists(directory):
-        os.mkdir(directory)
-
-
-def download_photo_by_media_id(my_bot: object, media_id: int, filename: str, directory_name: str) -> None:
+def download_photo_by_media_id(my_bot: object, media_id: int, filename: str) -> None:
     """
     :param my_bot: класс Bot из библиотеки instabot
     :param media_id: id поста, в которой возможно содержится фотография или крусель из фотографий
     :param filename: название, что будет приствоено фотографии
-    :param directory_name: директория, куда будут сохранены фотографии
-    :return:
     """
     media = my_bot.get_media_info(media_id)[0]
     if "image_versions2" in media.keys():
         url = media["image_versions2"]["candidates"][0]["url"]
         response = requests.get(url)
-        with open("photos" + "/" + directory_name + "/" + filename + ".jpg", "wb") as f:
+        image_name = os.path.join("photos", filename + ".jpg")
+        with open(image_name, "wb") as f:
             response.raw.decode_content = True
             f.write(response.content)
     elif "carousel_media" in media.keys():
-        for e, element in enumerate(media["carousel_media"]):
+        for i, element in enumerate(media["carousel_media"]):
             url = element['image_versions2']["candidates"][0]["url"]
             response = requests.get(url)
-            with open("photos" + "/" + directory_name + "/" + filename + str(e) + ".jpg", "wb") as f:
+            image_name = os.path.join("photos", filename + "_" + str(i) + ".jpg")
+            with open(image_name, "wb") as f:
                 response.raw.decode_content = True
                 f.write(response.content)
 
@@ -80,9 +72,8 @@ def get_all_photos(my_bot: object, nickname: str) -> None:
     :param nickname: имя пользователя в инстаграмме и имя директории, куда будут сохранены фотографии
     """
     twenty_last_medias = my_bot.get_total_user_medias(nickname)
-    os.mkdir("photos" + "/" + nickname)
-    for e, media_id in enumerate(twenty_last_medias):
-        download_photo_by_media_id(my_bot, media_id, filename="img_" + str(e), directory_name=nickname)
+    for i, media_id in enumerate(twenty_last_medias):
+        download_photo_by_media_id(my_bot, media_id, filename=nickname + str(i))
 
 
 def get_photo_by_url(my_bot: object, url: str) -> None:
@@ -93,5 +84,5 @@ def get_photo_by_url(my_bot: object, url: str) -> None:
     """
     # по url получаем id поста
     media_id = my_bot.get_media_id_from_link(url)
-    create_new_directory("another")
-    download_photo_by_media_id(my_bot, media_id, filename="test", directory_name="another")
+    filename = "my_choice" + str(time.time()).replace('.', '')
+    download_photo_by_media_id(my_bot, media_id, filename=filename)
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import functions
 
-user_url = "https://www.instagram.com/p/CSwTRIDoJlm/?utm_source=ig_web_copy_linkk"
+post_url = "https://www.instagram.com/p/CSpPlytISMS/?utm_source=ig_web_copy_link"
 target_name = "adim0k"
 
 
 if __name__ == "__main__":
-    functions.clean_up()
-    functions.clean_up(target_name)
     insta_bot = functions.connect()
     # functions.get_all_photos(insta_bot, target_name)
-    functions.get_photo_by_url(insta_bot, user_url)
+    functions.get_photo_by_url(insta_bot, post_url)
