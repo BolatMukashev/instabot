bbc74694423b2a5d4a695a01c204798de692a3fe
Bolat Mukashev <ya.ne.angel.kimi@gmail.com>
2021-12-03 16:52:16 +0500

    запуск

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from aiogram import executor
+from functions import create_base_directory
 from planner import on_startup
 
 
 if __name__ == "__main__":
     from bot import dp
+    create_base_directory()
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
